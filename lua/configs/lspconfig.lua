35c83d0f262480b524c5f7aed7917c04e4497593
Mikko Ryynänen <mikko.ryynanen2@gmail.com>
2025-09-23 15:09:34 +0300

    Initial commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,28 @@
+require("nvchad.configs.lspconfig").defaults()
+
+local servers = { "clangd", "lua" }
+vim.lsp.enable(servers)
+
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- Configure clangd
+lspconfig.clangd.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  },
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+  root_dir = require'lspconfig.util'.root_pattern("compile_commands.json", ".git")
+}
